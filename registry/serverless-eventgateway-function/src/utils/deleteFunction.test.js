d1a4b9b7aee7ed34830cc076a945c7b9cd4063e2
Philipp Muens <raute1337@gmx.de>
2018-09-14 13:50:10 +0200

    Add Event Gateway Function component

---
+++ registry/serverless-eventgateway-function/src/utils/deleteFunction.test.js
@@ -0,0 +1,35 @@
+const deleteFunction = require('./deleteFunction')
+
+const mockDeleteFunction = jest.fn().mockResolvedValue({
+  space: 'my-space',
+  functionId: 'myFunction',
+  provider: {
+    arn: 'arn:aws:lambda:us-east-1:12345:function:my-function',
+    region: 'us-east-1'
+  }
+})
+
+jest.mock('@serverless/event-gateway-sdk', () =>
+  jest.fn().mockImplementation(() => ({
+    deleteFunction: mockDeleteFunction
+  }))
+)
+
+describe('#deleteFunction()', () => {
+  it('should delete a function registration from the Event Gateway', async () => {
+    const inputs = {
+      url: 'http://localhost',
+      space: 'my-space',
+      accessKey: 's0m34cc355k3y',
+      functionId: 'myFunction',
+      type: 'awslambda',
+      provider: { arn: 'arn:aws:lambda:us-east-1:12345:function:my-function', region: 'us-east-1' }
+    }
+
+    const res = await deleteFunction(inputs)
+
+    const { functionId } = inputs
+    expect(mockDeleteFunction).toBeCalledWith({ functionId })
+    expect(res).toEqual(true)
+  })
+})
